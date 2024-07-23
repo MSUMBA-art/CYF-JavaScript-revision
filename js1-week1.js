e0621a2509baf1c2daa625897842c70146b965e3
ALEXANDER <msumba83@gmail.com>
2024-07-23 21:53:41 +0200

    first commit

---
+++ js1-week1.js
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+
+console.log(3 + 8);
+
+// ===== VARIABLES ====== 
+/*let greetings = "Hello World"
+console.log(greetings);
+
+const myName = "Alexander";
+console.log(myName);*/
+
+// Exercise;
+// 1. Add a variable greeting to js1-week1.js and assign a greeting of your choice to the variable
+
+// 2. Print your greeting to the console 3 times. You should see your greeting 3 times on the console, one on each line.
+
+// let greeting = "Good Morning";
+// console.log(greeting, greeting, greeting );
